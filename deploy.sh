7b4f222baf09d50907020aa3503e76abf93ddf52
William Merrill <vikingarnir.will@gmail.com>
2019-07-13 13:27:12 -0400

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Commit message.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# echo -e "\033[0;32mUpdating source on GitHub...\033[0m"
+# git commit -am '$msg'
+# git push origin master
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
@@ -11,10 +21,6 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
